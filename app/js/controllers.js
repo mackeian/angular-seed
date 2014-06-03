54bf3c7f909595aa15fc80d84355367174b264e1
Marcus Erlandsson <mail@marquz.com>
2014-06-03 15:33:35 +0200

    Added sample API and basic services for getting from the API

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,17 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', function($scope, SubjectService) {
+    SubjectService.get()
+    .then(
+      function(result) {
+        $scope.subject = result.data.payload;
+      },
+      function(result) {
+        alert('That did not go well!');
+      }
+    );
+  })
+  .controller('MyCtrl2', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+  });
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,13 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+.factory('SubjectService',
+  function($http) {
+    return {
+      get: function() {
+        return $http.get('/app/api/get_subject.json');
+      }
+    }
+  })
+.value('version', '0.1');
